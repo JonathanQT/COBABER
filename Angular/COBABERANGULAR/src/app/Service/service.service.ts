99ccd571ffc32cf8214b983c0fa1ced4fb7337cc
JQUIMBAYOT <jonathanquimbayo1996@gmail.com>
2020-06-08 00:26:10 -0500

    .

--- Angular/COBABERANGULAR/src/app/Service/service.service.ts
+++ Angular/COBABERANGULAR/src/app/Service/service.service.ts
@@ -2,15 +2,20 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Persona } from '../Modelo/Persona';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
+
   constructor(private http:HttpClient) { }
 
-  //Url='http://localhost:8080/demo/personas';
-  Url='/demo/personas';
+  //Url='demo/personas';
+  
+  Url='http://localhost:8080/andreslab/rest/opain/consultar';
+
+
   getPersonas(){    
 
     return this.http.get<Persona[]>(this.Url);
